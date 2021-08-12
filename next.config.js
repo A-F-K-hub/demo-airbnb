6f0c8a4b50527985f2b7e7c368df836bcd11a3ae
A-F-K-hub <62518244+A-F-K-hub@users.noreply.github.com>
2021-08-12 16:33:28 +0200

    build

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoibWRpa2kiLCJhIjoiY2tva2h4cHhxMDRjNjJvcWxhM21iMnZ6NSJ9.aCbSx4Go7xd4pf7e43Enlw",
+  },
 };
